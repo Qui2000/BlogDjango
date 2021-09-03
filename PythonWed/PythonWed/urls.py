38a48efef5813e7f7fbde74c607f3abb18a63126
Quang Qui <qui@liftsoft.vn>
2021-09-03 11:43:11 +0700

    update project

--- PythonWed/PythonWed/urls.py
+++ PythonWed/PythonWed/urls.py
@@ -15,13 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import handler404, handler500
-from django.conf.urls.static import static
+from django.conf.urls import handler404, handler500, url
+from django.conf.urls.static import static, serve
 from django.conf import settings
+
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('blog/', include('blog.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
